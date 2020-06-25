99d8e342d15bc449588014860d855cb1171db7a8
michaelvickdev <michael.vick.hello@gmail.com>
2020-06-25 17:13:01 +0200

    Learning Material-UI while creating a Modal Component
    Learning Material-UI while creating a Modal Component

--- src/context/Modal.context.js
+++ src/context/Modal.context.js
@@ -7,10 +7,28 @@ const ModalProvider = (props) => {
 
     const [idModalRecipe, setIdModalRecipeState] = useState(null);
 
+    const [apiAnswer, setApiAnswerState] = useState([]);
+
+    useEffect(() => {
+        if(!idModalRecipe) return;
+
+        const askingIdRecipeApi = async () =>{
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idModalRecipe}`
+            const res = await axios.get(url);
+
+            //console.log(res.data.drinks[0]);
+            setApiAnswerState(res.data.drinks[0]);
+
+        }
+        askingIdRecipeApi();
+    }, [idModalRecipe]);
+
+
     return(
         <ModalContext.Provider
         value={{
-            setIdModalRecipeState
+            setIdModalRecipeState,
+            apiAnswer
         }}>
           {props.children}
         </ModalContext.Provider>
