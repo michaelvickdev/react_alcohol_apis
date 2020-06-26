2209b266e6e177364981a3c87e72d7746abf66b5
michaelvickdev <michael.vick.hello@gmail.com>
2020-06-26 08:28:58 +0200

    Creating a model with Material-UI, and configuring all the close and open button, plus cleaning states
    Creating a model with Material-UI, and configuring all the close and open button, plus cleaning states

--- src/components/Recipe.component.js
+++ src/components/Recipe.component.js
@@ -1,9 +1,55 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {ModalContext} from '../context/Modal.context.js';
 
-    
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+
+  
+  function getModalStyle() {
+    const top = 50 ;
+    const left = 50 ;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 540,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+
+
+  ////////////////////////////////////
+  ////////////////////////////////////
+  
+  
+
 
 const RecipeComponent = props => {
+
+    // Material-UI
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     const {setIdModalRecipeState} = useContext(ModalContext);
 
@@ -19,10 +65,27 @@ const RecipeComponent = props => {
                 <button
                 type="button"
                 className="btn btn-primary btn-block"
-                onClick={() => setIdModalRecipeState(props.recipesState.idDrink)}
+                onClick={() => {
+                    setIdModalRecipeState(props.recipesState.idDrink);
+                    handleOpen();
+                }}
                 >
                     View Recipe
                 </button>
+
+                <Modal
+                    open={open}
+                    onClose={() =>{
+                        setIdModalRecipeState(null),
+                        handleClose();
+                    }}
+                >
+                <div style={modalStyle}
+                className={classes.paper}>
+                    <h1>From Modal</h1>
+                </div>
+
+                </Modal>
             </div>
 
         </div>
